e0ba9f712538917f9b87b714c8499090cee1bf9c
fragilefort <imaddex3759@gmail.com>
2025-12-31 20:54:41 +0100

    Changed some polybar features and lualine features in nvim

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -11,7 +11,6 @@ vim.g.have_nerd_font = true
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
-
 -- Make line numbers default
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
@@ -81,3 +80,5 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 vim.opt.clipboard = 'unnamedplus'
+
+vim.opt.guicursor = ""
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,22 +1,50 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "kanagawa", -- Automatically switches to dragon if colorscheme is dragon
-        globalstatus = true, -- One single statusline at the bottom for all splits
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-      },
-      sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' }, -- Here is your Git branch!
-        lualine_c = { { 'filename', path = 1 } },        -- Shows path so you don't get lost
-        lualine_x = { 'encoding', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
-      },
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		-- Custom "Sleepy Hollow" theme colors
+		local colors = {
+			bg       = '#16161D', -- Deep midnight black
+			fg       = '#C8C093', -- Faint parchment white
+			yellow   = '#7E9E73', -- Muted moss green
+			cyan     = '#7FB4CA', -- Misty blue
+			darkblue = '#223249', -- Deep water blue
+			gray     = '#2A2A37', -- Foggy gray
+			magenta  = '#957FB8', -- Pale twilight purple
+		}
+
+		local sleepy_hollow = {
+			normal = {
+				a = { fg = colors.bg, bg = colors.cyan, gui = 'bold' },
+				b = { fg = colors.fg, bg = colors.gray },
+				c = { fg = colors.fg, bg = colors.bg },
+			},
+			insert = { a = { fg = colors.bg, bg = colors.yellow, gui = 'bold' } },
+			visual = { a = { fg = colors.bg, bg = colors.magenta, gui = 'bold' } },
+			replace = { a = { fg = colors.bg, bg = colors.cyan, gui = 'bold' } },
+			inactive = {
+				a = { fg = colors.fg, bg = colors.bg },
+				b = { fg = colors.fg, bg = colors.bg },
+				c = { fg = colors.fg, bg = colors.bg },
+			},
+		}
+
+		require("lualine").setup({
+			options = {
+				theme = sleepy_hollow,
+				globalstatus = true,
+				-- Changed separators to vertical bars
+				component_separators = { left = '|', right = '|' },
+				section_separators = { left = '', right = '' },
+			},
+			sections = {
+				lualine_a = { 'mode' },
+				lualine_b = { 'branch', 'diff', 'diagnostics' },
+				lualine_c = { { 'filename', path = 1 } },
+				lualine_x = { 'encoding', 'filetype' },
+				lualine_y = { 'progress' },
+				lualine_z = { 'location' }
+			},
+		})
+	end,
 }
